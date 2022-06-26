e3351228d6c1107d6352be44d3806e65361b15b4
Antonio Flavio Mameli <86047200+AntoFM68@users.noreply.github.com>
2022-06-26 18:10:44 +0200

    Delete RL_Lab directory

--- RL_Lab/workspace/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import custom_envs
-from . import custom_modules
-from . import custom_models
-from . import configs
--- RL_Lab/workspace/custom_envs/mujoco_envs/envs/__init__.py
+++
@@ -1,2 +0,0 @@
-from custom_envs.mujoco_envs.envs.deepracer import Deepracer
-from custom_envs.mujoco_envs.envs.turtlebot import Turtlebot
